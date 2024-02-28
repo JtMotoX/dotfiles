1412679fbc0fb78ad6850365a378c395f1c71b2e
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2024-02-27 17:32:08 -0800

    add system nix packages

---
+++ packages.nix
@@ -0,0 +1,35 @@
+# LIST INSTALLED PACKAGES
+# nix-env -q
+
+# INSTALL PACKAGES.NIX
+# nix-env -f packages.nix -iA environment.systemPackages
+
+# REMOVE ALL PACKAGES
+# nix-env -e '*'
+
+######################################
+
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # inludes velero 1.9.3
+  velero_pin = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b3a285628a6928f62cdf4d09f4e656f7ecbbcafb.tar.gz") {};
+  # inludes kubectl 1.25.4
+  kubectl_pin = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b3a285628a6928f62cdf4d09f4e656f7ecbbcafb.tar.gz") {};
+in
+
+{
+  environment.systemPackages = with pkgs; [
+    git
+    kind
+    kubectl_pin.kubectl
+    yq-go
+    jq
+    velero_pin.velero
+    docker
+    zoxide
+    fzf
+    fzf-zsh
+    tmux
+  ];
+}
