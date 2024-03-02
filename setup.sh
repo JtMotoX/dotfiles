8d4e69e4fc37df6b1aad1bfdf7ff9bfb585841c9
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2024-03-02 11:33:53 -0800

    setup only reinstall nix if not installed

--- setup.sh
+++ setup.sh
@@ -11,9 +11,14 @@ if [ "$(uname)" = "Darwin" ]; then ./scripts/brew-install.sh; fi
 ./scripts/oh-my-zsh-install.sh
 ./scripts/tmux-configure.sh
 ./scripts/create-symbolic-links.sh
-./scripts/nix-uninstall.sh
-for key in $(env | grep '^NIX_' | awk -F= '{print $1}'); do unset $key; done
-./scripts/nix-install.sh
+
+# INSTALL NIX IF NOT EXISTS
+if ! command -v nix >/dev/null 2>&1 || [ ! -f '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ] || ! grep nixbld /etc/group >/dev/null 2>&1; then
+    ./scripts/nix-uninstall.sh
+    for key in $(env | grep '^NIX_' | awk -F= '{print $1}'); do unset $key; done
+    ./scripts/nix-install.sh
+fi
+
 if [ -f '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'; fi
 ./scripts/nix-packages-uninstall.sh
 ./scripts/nix-packages-install.sh
