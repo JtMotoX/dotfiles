bc8dc711db02b7bb04b9e79486cbf7a55724b7a8
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2024-03-02 11:34:35 -0800

    add more package managers support for zsh install

--- scripts/zsh-install.sh
+++ scripts/zsh-install.sh
@@ -8,13 +8,21 @@ if command -v zsh >/dev/null 2>&1; then
 else
 	echo "Installing zsh"
 	if command -v apt-get >/dev/null 2>&1; then
-		sudo apt-get install -y zsh
+		sudo apt-get update && apt-get install -y zsh
 	elif command -v pacman >/dev/null 2>&1; then
 		sudo pacman -S zsh
 	elif command -v dnf >/dev/null 2>&1; then
 		sudo dnf install -y zsh
 	elif command -v brew >/dev/null 2>&1; then
 		brew install zsh
+	elif command -v port >/dev/null 2>&1; then
+		sudo port install zsh zsh-completions
+	elif command -v zypper >/dev/null 2>&1; then
+		sudo zypper install zsh
+	elif command -v yum >/dev/null 2>&1; then
+		sudo yum install -y zsh
+	elif comand -v apk >/dev/null 2>&1; then
+		sudo apk add zsh
 	else
 		echo "Please install zsh with your package manager"
 		exit 1
