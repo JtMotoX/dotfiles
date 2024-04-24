0e0b8c121cb59da26b8e54e3150ec75ec7f2b6c2
Jonathan Fair <jonathaf@netapp.com>
2024-04-24 03:14:00 +0000

    support custom nix uid range

--- scripts/nix-install.sh
+++ scripts/nix-install.sh
@@ -8,6 +8,21 @@ if mount | grep -E '\/tmp\s.*noexec' >/dev/null 2>&1; then
     sudo mount -o remount,exec /tmp
 fi
 
+# TRY A DIFFERENT UID IF ALREADY TAKEN
+if [ "${NIX_FIRST_BUILD_UID}" = "" ]; then
+    if getent passwd 30001 >/dev/null 2>&1; then
+        if getent passwd 300001 >/dev/null 2>&1; then
+            echo "UID's 30001 and 300001 both already exists"
+            exit 1
+        else
+            export NIX_FIRST_BUILD_UID=300001
+        fi
+    fi
+fi
+if [ "${NIX_FIRST_BUILD_UID}" != "" ]; then
+    echo "Using custom NIX UID RANGE: '${NIX_FIRST_BUILD_UID}'"
+fi
+
 # INSTALL NIX
 echo "Installing nix . . ."
 if [ "$(uname)" = "Darwin" ]; then
@@ -22,6 +37,8 @@ elif [ "$(uname)" = "Linux" ]; then
 else
     echo "This is not macOS or Linux"
 fi
+
+unset NIX_FIRST_BUILD_UID
 
 if [ ! -f ~/.nix-profile/etc/profile.d/nix.sh ] && [ ! -f '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
     echo "Failed to install nix"
